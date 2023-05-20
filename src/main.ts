0c3f15e6c890f608f8f1bd7676d8f3a74e17658d
Juan Pipa <jmpipah@gmail.com>
2023-05-19 21:54:39 -0500

    Documentacion
    - Instalacion de Swagger
    - Configuracion de Swagger en el main.ts
    - Agregacion de decorador ApiProperty de Swagger para la documentacion de nuestra ApiRest

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { ValidationPipe } from "@nestjs/common";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -15,6 +16,10 @@ async function bootstrap() {
       },
     }),
   );
+
+  const config = new DocumentBuilder().setTitle("API").setDescription("Autenticación y Autorización con NestJS y MongoDB").setVersion("1.0").build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup("docs", app, document);
 
   app.enableCors();
   await app.listen(4000);
--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,8 +1,10 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsEmail, IsString, IsNotEmpty, Matches, MinLength, MaxLength } from "class-validator";
 
 export class CreateUserDto {
   @IsNotEmpty({ message: "El email es requerido" })
   @IsEmail({}, { message: "Este campo debe ser un email válido" })
+  @ApiProperty()
   readonly email: string;
 
   @IsString({ message: "La contraseña debe contener caractéres válidos" })
@@ -11,13 +13,16 @@ export class CreateUserDto {
   @Matches(/(?:(?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
     message: "La contraseña debe tener letras mayúsculas, min+usculas y números",
   })
+  @ApiProperty()
   password: string;
 
   @IsString()
   @IsNotEmpty({ message: "Los nombres son requeridos" })
+  @ApiProperty()
   firstName: string;
 
   @IsString()
   @IsNotEmpty({ message: "Los apellidos son requeridos" })
+  @ApiProperty()
   lastName: string;
 }
